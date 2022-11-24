06a48ba5802223dcd12f4890cdde637f00fc5b7c
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2022-11-24 17:05:46 -0500

    State handling is centralised in StateManager.
    Also cleaned up and added documentation.

--- Assets/Scripts/PlayerStateHandler.cs
+++ Assets/Scripts/PlayerStateHandler.cs
@@ -4,85 +4,62 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// Editors:				Szun Kidd Choi
+// Date Created:		11/24/22
+// Date Last Edited:	11/24/22
+
 public class PlayerStateHandler : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] private GameObject recyclingPosition;
-    [SerializeField] private GameObject playerCapsule;
-    [SerializeField] private Camera recyclingCamera;
-    [SerializeField] private Camera mainCamera;
-    private PlayerInput playerInput;
-    public bool inRecyclingTrigger;
+    /// <summary>
+    /// Flags whether the player is in proximity to be able to play the 
+    /// recycling minigame.
+    /// </summary>
+    private bool inRecyclingTrigger;
+
+    /// <summary>
+    /// Internal state, used to update the StateManager based on player actions.
+    /// </summary>
     public GameStates State { get; private set; } = GameStates.Collecting;
-    public Transform toReceptacles;
 
     private void Start()
     {
-        playerInput = GetComponent<PlayerInput>();
         inRecyclingTrigger = false;
-        recyclingCamera.enabled = false;
-    }
-
-    public void LookAtReceptacles()
-    {
-        // TODO: move to centre of collider
-        Debug.Log("lookatreceptacles");
-        // Turn player to look at receptacles.
-        playerCapsule.transform.LookAt(toReceptacles);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log(State);
-
         // I could have just set the state here but I wanted to explicitly handle
         // all state-related stuff in the StateManager itself.
+
+        // Player is near the bins, and can choose to play the recycling
+        // minigame.
         if (other.gameObject.CompareTag("RecyclingTrigger"))
             inRecyclingTrigger = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
+        // Player is too far away from the bins, and cannot play the recycling
+        // minigame.
         if (other.gameObject.CompareTag("RecyclingTrigger"))
             inRecyclingTrigger = false;
     }
 
-    private void OnRecycle(InputValue startRecyclingValue)
-    {
-        //Debug.Log("recycling");
-        State = GameStates.Recycling;
-    }
-
+    /// <summary>
+    /// Listen for ENTER keypress to switch states to Recycling if player is
+    /// close enough to the bins.
+    /// </summary>
     private void OnEnableRecycling(InputValue enableRecyclingValue)
     {
-        //controls.playerCount 
-        if (inRecyclingTrigger)
-        {
-            Debug.Log("recycling enabled");
-
-            // To manually set the position of a GameObject with a
-            // CharacterController attached, disable the CharacterController,
-            // move the object, then re-enable the CharacterController.
-            GetComponent<CharacterController>().enabled = false; //gameObject.SetActive(false);
-            playerCapsule.transform.position = new Vector3(
-                recyclingPosition.transform.position.x,
-                recyclingPosition.transform.position.y,
-                playerCapsule.transform.position.z);
-            //LookAtReceptacles();
-            GetComponent<CharacterController>().enabled = true; //gameObject.SetActive(true);
-
-            mainCamera.enabled = !mainCamera.enabled;
-            recyclingCamera.enabled = !recyclingCamera.enabled;
-
-            playerInput.SwitchCurrentActionMap("Recycling");
-        }
+        if (inRecyclingTrigger) State = GameStates.Recycling;
     }
 
+    /// <summary>
+    /// Listen for ENTER keypress to switch states to Collecting if player is
+    /// playing the recycling minigame.
+    /// </summary>
     private void OnEnablePlayer(InputValue enablePlayerValue)
     {
-        playerInput.SwitchCurrentActionMap("Player");
-        mainCamera.enabled = !mainCamera.enabled;
-        recyclingCamera.enabled = !recyclingCamera.enabled;
-        Debug.Log("player enabled");
+        State = GameStates.Collecting;
     }
 }
--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Editors:				Szun Kidd Choi
+// Date Created:		11/24/22
+// Date Last Edited:	11/24/22
 
 public enum GameStates
 {
@@ -11,35 +16,64 @@ public enum GameStates
 
 public class StateManager : MonoBehaviour
 {
+    /// <summary>
+    /// Static singleton instance of this class.
+    /// </summary>
     private static StateManager instance;
-    [SerializeField] private GameObject player;
-    [SerializeField] private PlayerStateHandler playerStateHandler;
+
+    [SerializeField, Tooltip("The camera attached to the first person controller.")] 
+    private Camera mainCamera;
+
+    [SerializeField, Tooltip("The camera active during the recycling minigame.")] 
+    private Camera recyclingCamera;
+
+    [SerializeField, Tooltip("The capsule representing the player character.")] 
+    private GameObject playerCapsule;
+
+    [SerializeField, Tooltip("The script handling changes of state within the " +
+        "player object.")] 
+    private PlayerStateHandler playerStateHandler;
+    
+    [SerializeField, Tooltip("The position the player resets to after exiting " +
+        "the recycling minigame")] 
+    private GameObject recyclingPosition;
+    
+    [SerializeField, Tooltip("Reference to the Player Input component attached " +
+        "to the player capsule")] 
+    private PlayerInput playerInput;
+
+    [SerializeField, Tooltip("Arrow GameObject showing the nearest piece of " +
+        "trash to the player in the scene.")] 
+    private GameObject arrow;
 
     public GameStates State { get; set; }
 
+    /// <summary>
+    /// Getter for the static singleton instance.
+    /// </summary>
     public static StateManager Instance
     {
         get
         {
-            //// Search for a component of type T in the Scene.
-            ////
-            //// If FindObjectOfType() returns null, then the component does
-            //// not exist in the scene yet. Create an empty GameObject and
-            //// attach the component to it, since all executable code must
-            //// be attached to an active GameObject within the Hierarchy.
-            ////
-            //// This way, we "lazily" instantiate the singleton (i.e. only
-            //// create it when it is needed. Therefore, the component, and
-            //// therefore the GameObject it needs to be attached to, do not
-            //// need to exist in the Scene beforehand.
-            //if (!instance)
-            //{
-            //    instance = FindObjectOfType<StateManager>();
+            // Search for a component of type StateManager in the Scene.
+            //
+            // If FindObjectOfType() returns null, then the component does
+            // not exist in the scene yet. Create an empty GameObject and
+            // attach the component to it, since all executable code must
+            // be attached to an active GameObject within the Hierarchy.
+            //
+            // This way, we "lazily" instantiate the singleton (i.e. only
+            // create it when it is needed. Therefore, the component, and
+            // therefore the GameObject it needs to be attached to, do not
+            // need to exist in the Scene beforehand.
+            if (!instance)
+            {
+                instance = FindObjectOfType<StateManager>();
 
-            //    GameObject gameObject = new GameObject();
-            //    gameObject.name = typeof(StateManager).Name;
-            //    instance = gameObject.AddComponent<StateManager>();
-            //}
+                GameObject gameObject = new GameObject();
+                gameObject.name = typeof(StateManager).Name;
+                instance = gameObject.AddComponent<StateManager>();
+            }
 
             return instance;
         }
@@ -48,11 +82,14 @@ public class StateManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Ensure the State Manager persists between gameplay and menu screens.
         DontDestroyOnLoad(this);
 
         // We don't have a menu screen yet, so just begin with the collection
         // phase for now.
         State = GameStates.Collecting;
+
+        recyclingCamera.enabled = false;
     }
 
     // Update is called once per frame
@@ -60,23 +97,66 @@ public class StateManager : MonoBehaviour
     {
         switch (State)
         {
+            // TODO: Implement this when Menu UI is added.
             case GameStates.Menu:
                 Debug.LogError("This shouldn't happen.");
                 break;
+
+            // Collecting phase
             case GameStates.Collecting:
+                // Trigger is player state handler changing state.
+                // Switch to recycling state.
                 if (playerStateHandler.State == GameStates.Recycling)
                 {
-                    Debug.Log("turning");
-                    // Turn player to look at receptacles.
-                    playerStateHandler.LookAtReceptacles();
-
-                    Debug.Log("switching state");
+                    //Debug.Log("switching state");
                     State = GameStates.Recycling;
+
+                    // To manually set the position of a GameObject with a
+                    // CharacterController attached, disable the CharacterController,
+                    // move the object, then re-enable the CharacterController.
+                    playerCapsule.GetComponent<CharacterController>().enabled = false;
+                    playerCapsule.transform.position = new Vector3(
+                        recyclingPosition.transform.position.x,
+                        recyclingPosition.transform.position.y,
+                        playerCapsule.transform.position.z);
+                    playerCapsule.GetComponent<CharacterController>().enabled = true;
+
+                    // Don't show the arrow during the recycling minigame
+                    arrow.SetActive(false);
+
+                    // Switch cameras.
+                    mainCamera.enabled = !mainCamera.enabled;
+                    recyclingCamera.enabled = !recyclingCamera.enabled;
+
+                    // Switch input action maps to listen for inputs relevant
+                    // to the recycling minigame.
+                    playerInput.SwitchCurrentActionMap("Recycling");
                 }
 
                 break;
+
+            // Recycling minigame
             case GameStates.Recycling:
-                // trigger to switch to collecting
+                // Trigger is player state handler changing state.
+                // Switch to collecting state.
+                if (playerStateHandler.State == GameStates.Collecting)
+                {
+                    //Debug.Log("switching state");
+                    State = GameStates.Collecting;
+
+                    // Switch input action maps to listen for inputs relevant
+                    // to the collecting phase.
+                    playerInput.SwitchCurrentActionMap("Player");
+
+                    // Re-display the arrow.
+                    arrow.SetActive(true);
+
+                    // Switch cameras.
+                    mainCamera.enabled = !mainCamera.enabled;
+                    recyclingCamera.enabled = !recyclingCamera.enabled;
+                    //Debug.Log("player enabled");
+                }
+
                 break;
         }
     }
