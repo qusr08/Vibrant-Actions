0d100261e6b0f4dd8514d58d6f65d0ff1878d470
YuLingDong <54948704+YuLingDong@users.noreply.github.com>
2022-12-04 17:09:21 -0500

    adjusted size, color, location of instructions UI

--- Assets/Scripts/ShowUI.cs
+++ Assets/Scripts/ShowUI.cs
@@ -5,21 +5,20 @@ using System.Linq;
 
 public class ShowUI : MonoBehaviour
 {
-    // public GameObject ui;
-    // public GameObject[] ui = GameObject.FindGameObjectsWithTag("instruct");
     public List<GameObject> ui = new List <GameObject>();
     public bool collect; 
 
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(Wait());
         ui = GameObject.FindGameObjectsWithTag("Instruct").ToList();
 
-        ui[0].SetActive(true);
-        for(int i = 1; i<6; i++){
+        for(int i = 0; i<7; i++){
             ui[i].SetActive(false);
         }
-        
+        ui[1].SetActive(true);
+
         StartCoroutine(Wait());
     }
 
@@ -29,45 +28,40 @@ public class ShowUI : MonoBehaviour
         //Check for movement key pressed
         if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
         {
-            ui[0].SetActive(false);
-            ui[1].SetActive(true);
-            StartCoroutine(Wait());
-            
-            //Mouse Movement
             ui[1].SetActive(false);
             ui[2].SetActive(true);
             StartCoroutine(Wait());
-
-            //Trash pickup
+            
+            //Mouse Movement
             ui[2].SetActive(false);
             ui[3].SetActive(true);
+            StartCoroutine(Wait());
+
+            //Trash pickup
+            ui[3].SetActive(false);
             ui[4].SetActive(true);
+            ui[5].SetActive(true);
             StartCoroutine(Wait());
 
             //Check that a piece of trash is picked up
             if(collect)
             {
                 //Prompt for trash disposal
-                ui[3].SetActive(false);
                 ui[4].SetActive(false);
-                ui[5].SetActive(true);
-                StartCoroutine(Wait());
-
-                //Cheers
                 ui[5].SetActive(false);
                 ui[6].SetActive(true);
                 StartCoroutine(Wait());
-            }
 
+                //Cheers
+                ui[6].SetActive(false);
+                ui[7].SetActive(true);
+                StartCoroutine(Wait());
+            }
         }
     }
 
     IEnumerator Wait()
     {
         yield return new WaitForSeconds(3.0f);
-        //Destroy(ui);
-        //Destroy script to prevent showing the text again
-        //Destroy();
     }
-
 }
